8b6bc9fdbfa155a514bf55d1b01b6167e385bfcf
Brandi <blubrandi0@gmail.com>
2019-12-18 09:28:08 -0700

    Removed Xcode project and recreated as a framework with Unit Tests

---
+++ RPN-Calculator/RPN-Calculator/RPN_Calculator.h
@@ -0,0 +1,19 @@
+//
+//  RPN_Calculator.h
+//  RPN-Calculator
+//
+//  Created by Brandi Bailey on 12/18/19.
+//  Copyright © 2019 Brandi Bailey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RPN_Calculator.
+FOUNDATION_EXPORT double RPN_CalculatorVersionNumber;
+
+//! Project version string for RPN_Calculator.
+FOUNDATION_EXPORT const unsigned char RPN_CalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPN_Calculator/PublicHeader.h>
+
+
---
+++ RPN-Calculator/RPN-CalculatorTests/RPN_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  RPN_CalculatorTests.swift
+//  RPN-CalculatorTests
+//
+//  Created by Brandi Bailey on 12/18/19.
+//  Copyright © 2019 Brandi Bailey. All rights reserved.
+//
+
+import XCTest
+@testable import RPN_Calculator
+
+class RPN_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
