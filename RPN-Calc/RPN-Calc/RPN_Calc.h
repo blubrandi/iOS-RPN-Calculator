60321886c5fa2a9dc2e1fac02b86baffffd2de03
Brandi <blubrandi0@gmail.com>
2019-12-18 10:23:34 -0700

    Re-created Framework with Unit Tests

---
+++ RPN-Calc/RPN-Calc/RPN_Calc.h
@@ -0,0 +1,19 @@
+//
+//  RPN_Calc.h
+//  RPN-Calc
+//
+//  Created by Brandi Bailey on 12/18/19.
+//  Copyright © 2019 Brandi Bailey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RPN_Calc.
+FOUNDATION_EXPORT double RPN_CalcVersionNumber;
+
+//! Project version string for RPN_Calc.
+FOUNDATION_EXPORT const unsigned char RPN_CalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPN_Calc/PublicHeader.h>
+
+
